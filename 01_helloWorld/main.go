0eb3e9290eb4b70fe4f723680145b945464af506
GameKingNoob <66spammail55@gmail.com>
2016-03-06 08:58:59 +0100

    first commit

---
+++ 01_helloWorld/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+// ctr + k clears the Terminal
+
+// cd 01_helloWorld requird to be able to run -> go fmt
+// path has to end with \01_helloWorld
